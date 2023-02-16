4a9b9dabe33afbd8a6149cfb96fb2c094efebb9e
Adewale <jamiuadewaleyusuf@gmail.com>
2023-02-16 12:47:49 -0800

    display an output without using printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print a given message followed by a standard error
+ *
+ * Return: 1 if successful
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
