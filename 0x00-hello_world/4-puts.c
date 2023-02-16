45b1ae83acef45b16bbb5bba927beba40216cead
Adewale <jamiuadewaleyusuf@gmail.com>
2023-02-16 08:24:59 -0800

    write a c program that prints the given statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints using puts
+ *
+ * Return: 0 if successfull
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
+
+}
