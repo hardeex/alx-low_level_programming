a91bad8420051d61c579ed1e32ed9494bd211b75
Adewale <jamiuadewaleyusuf@gmail.com>
2023-02-17 12:04:52 -0800

    print single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- A program that prints single digits
+ *
+ * Return: 0 if successful
+ * Author: Adewale
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; number++)
+		putchar(number);
+
+	return (0);
+}
+
