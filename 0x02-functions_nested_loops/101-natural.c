3a8cb243712cbb9b5dfda843f786e222a07c0509
Adewale <jamiuadewaleyusuf@gmail.com>
2023-02-22 11:04:55 -0800

    A program that print natural numbers below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- A program that list all natural
+ * numbers below 1024
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+		printf("%d\n", sum);
+		return (0);
+}
