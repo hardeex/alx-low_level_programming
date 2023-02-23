a0bbb5143ae86cf47f54066099b8bb9cc42679e4
Adewale <jamiuadewaleyusuf@gmail.com>
2023-02-23 06:37:06 -0800

    A function that prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line- A function that drwas a straiight line
+ * @c: The argument
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+	else
+		_putchar('\n');
+}
+
