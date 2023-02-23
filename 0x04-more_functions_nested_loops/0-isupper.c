6b2c4d7449781095015767cfc600892f565ec0dd
Adewale <jamiuadewaleyusuf@gmail.com>
2023-02-23 04:19:07 -0800

    A program that checks for uppercase char

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper- A program that checks for uppercase character
+ * @c: The argument
+ *
+ * Return: 1 if successful and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
