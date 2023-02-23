6f9c7541340712394cfe293a1ca9fc63e7cf0bbd
Adewale <jamiuadewaleyusuf@gmail.com>
2023-02-23 08:35:54 -0800

    A program that print 1 to 100 and fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main- A program that print 1 to 100
+ * print Fizz for multiples of 3
+ * print Buzz for the multiples of 5
+ * print FizzBuzz for the multiples of 3 and 5
+ *
+ * Return: 0 if successful
+ *
+ * Author: Adewale
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 100; digit++)
+	{
+		if ((digit % 3) == 0 && (digit % 5) == 0)
+			printf("FizzBuzz");
+		else if ((digit % 3) == 0)
+			printf("Fzz");
+		else if ((digit % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", digit);
+		if (digit == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
