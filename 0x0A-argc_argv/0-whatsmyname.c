f4dfa9f05be5efbf0c7823f2c428add321e2050e
Adewale <jamiuadewaleyusuf@gmail.com>
2023-03-10 08:57:37 -0800

    A function that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- A function that prints its name, followed by a new line
+ * @argc: The argument that counts the variables
+ * @argv: The argument that stores the argrc
+ * Return: 0 if successful
+ * Student: Adewale
+ */
+int main(argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("The added name = %s\n", argv[0]);
+		return (0)
+	}
+}
