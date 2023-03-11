07e41c5a23132a72d38c94b09827559b67882eb2
Adewale <jamiuadewaleyusuf@gmail.com>
2023-03-11 14:50:12 -0800

    adding the .c files

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper- A function that checks for uppercase in a string
+ * @c: The function argument
+ *
+ * Return: 0 if successful
+ * Student: Adewale
+ */
+int _isupper(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
